13a9babf073b9eae33ac3e1ec94b0d0b05f14c41
Carlos <cadusouza14@gmail.com>
2021-08-24 15:24:34 -0300

    Removed support for task nesting, for now

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,15 @@ mod tasks;
 use tasks::{TaskItem, TaskList};
 
 fn main() {
-    // let mut tl = TaskList::new("New List");
-    // let mut c = TaskItem::new("Test", "Test2");
-    // c.add_child(TaskItem::new("C", "C2"));
-    // c.get_task(0)
-    //     .unwrap()
-    //     .add_child(TaskItem::new("Inside C", "Inside C2"));
-    // c.get_task(0)
-    //     .unwrap()
-    //     .get_task(0)
-    //     .unwrap()
-    //     .add_child(TaskItem::new("Inside Inside C", "Inside Inside C2"));
+    let mut tl = TaskList::new("Test");
 
-    // c.add_child(TaskItem::new("Second", "Debug"));
-    // tl.add_task(c);
+    tl.add_task(
+        TaskItem::new("title: &str", "description: &str", true)
+    );
 
-    // // tl.add_task(c);
-    // // println!("{:#?}", tl);
-    // // tl.change_task(0, TaskItem::new("Test new", "Test2 new"), true);
+    println!("{}", tl.format_tasks(1));
 
-    // println!("{}", tl.format_tasks(0, 4));
-    // // println!("{:#?}", c);
-    let a = (1, "A");
+    tl.remove_task(0);
+    
+    println!("{}", tl.format_tasks(1));
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,94 +1,55 @@
-// TODO: Create an id system to retrieve objects easily.
-// TODO: Remove Option<&mut TaskItem> as the return type of get_task()
-#[derive(Debug)]
 pub struct TaskItem {
     title: String,
     description: String,
-    is_completed: bool,
-    subtasks: Vec<TaskItem>,
+    is_completed: bool,   
 }
 
 impl TaskItem {
-    pub fn new(title: &str, description: &str) -> TaskItem {
+    pub fn new(title: &str, description: &str, is_completed: bool) -> TaskItem {
         return TaskItem {
             title: title.to_string(),
             description: description.to_string(),
-            is_completed: false,
-            subtasks: Vec::new(),
+            is_completed: is_completed
         };
     }
 
-    pub fn add_child(&mut self, child: TaskItem) {
-        self.subtasks.push(child);
-    }
-
-    pub fn get_task(&mut self, index: usize) -> Option<&mut TaskItem> {
-        return if self.subtasks.is_empty() {
-            None
-        } else {
-            Some(&mut self.subtasks[index])
-        };
-    }
-
-    pub fn format_tasks(&self, depth: usize, max_depth: usize) -> String {
+   pub fn format(&self, depth: usize) -> String {
         return format!(
-            "\n{}[{}] {} {} {}",
+            "{}[{}] {} {}",
             std::iter::repeat("\t").take(depth).collect::<String>(),
-            if self.is_completed { "O" } else { "X" },
+            if self.is_completed { "X" } else { " " },
             self.title,
-            self.description,
-            if depth == max_depth || self.subtasks.is_empty() {
-                "".to_string()
-            } else {
-                self.subtasks
-                    .iter()
-                    .map(|item| item.format_tasks(depth + 1, max_depth))
-                    .collect()
-            }
-        );
+            self.description,   
+        )
     }
 }
 
-#[derive(Debug)]
 pub struct TaskList {
     name: String,
-    tasks:Vec<TaskItem>,
+    tasks: Vec<TaskItem>
 }
 
 impl TaskList {
-    pub fn new(name: &str) -> TaskList {
-        return TaskList {
+    pub fn new(name: &str) -> Self {
+        return Self {
             name: name.to_string(),
-            tasks: Vec::new(),
-        };
-    }
-
-    pub fn add_task(&mut self, task: TaskItem) {
-        self.tasks.push(task);
-    }
-
-    pub fn get_task(&self, index: usize) -> &TaskItem {
-        return &self.tasks[index];
-    }
-
-    pub fn change_task(&mut self, index: usize, new_task: TaskItem, keep_subtasks: bool) {
-        self.tasks[index].title = new_task.title;
-        self.tasks[index].description = new_task.description;
-        self.tasks[index].is_completed = new_task.is_completed;
-
-        if !keep_subtasks {
-            self.tasks[index].subtasks = vec![]
+            tasks: Vec::new()
         }
     }
 
-    pub fn format_tasks(&self, depth: usize, max_depth: usize) -> String {
+    pub fn format_tasks(&self, depth: usize) -> String {
         return format!(
-            "{}: {}",
+            "{}: \n{}",
             self.name,
-            self.tasks
-                .iter()
-                .map(|task| task.format_tasks(depth + 1, max_depth))
-                .collect::<String>()
-        );
+            self.tasks.iter().map(|task| task.format(depth)).collect::<String>()
+        )
+    }
+
+    pub fn add_task(&mut self, task: TaskItem) {
+        self.tasks.push(task)
+    }
+
+    pub fn remove_task(&mut self, index: usize) -> TaskItem {
+        self.tasks.remove(index)
     }
 }
