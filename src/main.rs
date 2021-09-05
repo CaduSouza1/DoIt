9d9b8c2c3ec19c6a0bd91f0542124cc205d2ed4a
Carlos <cadusouza14@gmail.com>
2021-09-05 18:35:25 -0300

    Added some commands related to adding and removing of lists and tasks

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,159 @@
+mod cli;
 mod tasks;
 
-use tasks::{TaskItem, TaskList};
+use std::io::Write;
+use std::{collections::HashMap, fs::File, io::BufReader};
+use tasks::{Program, TaskItem, TaskList};
 
-fn main() {
-    let mut tl = TaskList::new("Test");
-
-    tl.add_task(
-        TaskItem::new("title: &str", "description: &str", true)
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = clap::App::new("DoIt")
+    .after_help("The filepath and list names are reused after the first time using them. So it is not necessary to always specify these arguments")
+    .arg(
+        clap::Arg::with_name("filepath")
+        .short("f")
+        .long("file")
+        .help("Sets the file to make the necessary operations")
+        .value_name("Filepath"),
+    )
+    .arg(
+        clap::Arg::with_name("new-list")
+        .short("n")
+        .long("new-list")
+        .help("Creates a new task list with a specified by the -l command")
+    )
+    .arg(
+        clap::Arg::with_name("add-task")
+        .short("a")
+        .long("add-task")
+        .help("Adds a new task and appends it to a task list")
+        .takes_value(true)
+        .multiple(true)
+        .value_names(&["Title", "Description", "Is completed"])
+    )
+    .arg(
+        clap::Arg::with_name("delete-list")
+        .short("d")
+        .long("delete-list")
+        .help("Deletes a list specified by the -l command")
+    )
+    .arg(
+        clap::Arg::with_name("remove-task")
+        .short("r")
+        .long("remove-task")
+        .help("Removes a task at a specific index on a task list")
+        .takes_value(true)
+        .value_name("Index")
+    )
+    .arg(
+        clap::Arg::with_name("list")
+        .short("l")
+        .long("list")
+        .help("Sets the task list to make the necessary operations")
+        .takes_value(true)
+        .value_name("List name")
+    )
+    .arg(
+        clap::Arg::with_name("show")
+        .short("s")
+        .long("show")
+        .help("Shows all created tasks of a particular list")
+    )
+    .arg(
+        clap::Arg::with_name("show-all")
+        .long("show-all")
+        .help("Shows all created tasks")
     );
 
-    println!("{}", tl.format_tasks(1));
+    let defaults_filepath = "defaults.json";
 
-    tl.remove_task(0);
+    let mut defaults: HashMap<String, String> =
+        serde_json::from_reader(BufReader::new(&File::open(&defaults_filepath)?))?;
+
+    let matches = app.get_matches();
+
+    let filepath = match matches.value_of("filepath") {
+        Some(path) => path.to_string(),
+        None => {
+            let path = defaults.get("last_filepath").unwrap();
+            println!("No filepath given. Using last filepath \"{}\"", path);
+
+            path.clone()
+        }
+    };
+
+    let mut program = Program::load_from_json_file(&File::open(&filepath)?)?;
+
+    let list_name = match matches.value_of("list") {
+        Some(name) => name.to_string(),
+        None => {
+            let name = defaults.get("last_list_name").unwrap();
+            println!("No list name given. Using default list name \"{}\"", name);
+            name.clone()
+        }
+    };
+
+    matches.values_of("add-task").and_then(|mut args| {
+        println!("Added task to list {}", list_name);
+        program.add_task(
+            &list_name,
+            TaskItem::new(args.nth(0).unwrap(), args.nth(0).unwrap(), false),
+        )
+    });
+
+    matches.value_of("remove-task").and_then(|index| {
+        println!("Removed task {} from list {}", index, list_name);
+        program.remove_task(&list_name, index.parse().unwrap())
+    });
     
-    println!("{}", tl.format_tasks(1));
+    if matches.is_present("new-list") {
+        println!("Created list with name {}", list_name);
+        program.add_list(list_name.clone(), TaskList::default());
+    };
+    
+    if matches.is_present("delete-list") {
+        println!("Deleted list with name {}", list_name);
+        program.remove_list(&list_name);
+    };
+
+    if matches.is_present("show") {
+        println!("{}", program.format_task_list(&list_name, 1).unwrap());
+    }
+
+    if matches.is_present("show-all") {
+        println!("{}", program.format_all_tasks(1).unwrap());
+    }
+
+    defaults
+        .insert("last_filepath".to_string(), filepath.clone())
+        .and_then(|old_filepath| {
+            if old_filepath != filepath {
+                println!(
+                    "Updated default filepath from {} to {}",
+                    old_filepath, filepath
+                )
+            }
+
+            Some(())
+        });
+
+    defaults
+        .insert("last_list_name".to_string(), list_name.clone())
+        .and_then(|old_list_name| {
+            if old_list_name != list_name {
+                println!(
+                    "Updated default list name from {} to {}",
+                    old_list_name, list_name
+                )
+            }
+
+            Some(())
+        });
+
+    let mut program_file_write = File::create(&filepath)?;
+    let mut defaults_file_write = File::create(&defaults_filepath)?;
+
+    program_file_write.write_all(serde_json::to_string_pretty(&program)?.as_bytes())?;
+    defaults_file_write.write_all(serde_json::to_string_pretty(&defaults)?.as_bytes())?;
+
+    Ok(())
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,48 +1,57 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+// use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
 pub struct TaskItem {
     title: String,
     description: String,
-    is_completed: bool,   
+    is_completed: bool,
 }
 
 impl TaskItem {
-    pub fn new(title: &str, description: &str, is_completed: bool) -> TaskItem {
-        return TaskItem {
+    pub fn new(title: &str, description: &str, is_completed: bool) -> Self {
+        return Self {
             title: title.to_string(),
             description: description.to_string(),
-            is_completed: is_completed
+            is_completed: is_completed,
         };
     }
 
-   pub fn format(&self, depth: usize) -> String {
+    pub fn format(&self, depth: usize) -> String {
         return format!(
-            "{}[{}] {} {}",
+            "{}[{}] {} \n{}{}\n\n",
             std::iter::repeat("\t").take(depth).collect::<String>(),
             if self.is_completed { "X" } else { " " },
             self.title,
-            self.description,   
-        )
+            std::iter::repeat("\t").take(depth + 1).collect::<String>(),
+            self.description
+        );
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct TaskList {
-    name: String,
-    tasks: Vec<TaskItem>
+    tasks: Vec<TaskItem>,
 }
 
 impl TaskList {
-    pub fn new(name: &str) -> Self {
-        return Self {
-            name: name.to_string(),
-            tasks: Vec::new()
+    pub fn default() -> Self {
+        Self {
+            tasks: Vec::default(),
         }
     }
 
     pub fn format_tasks(&self, depth: usize) -> String {
         return format!(
-            "{}: \n{}",
-            self.name,
-            self.tasks.iter().map(|task| task.format(depth)).collect::<String>()
-        )
+            "{}\n",
+            self.tasks
+                .iter()
+                .map(|task| task.format(depth))
+                .collect::<String>()
+        );
     }
 
     pub fn add_task(&mut self, task: TaskItem) {
@@ -51,5 +60,55 @@ impl TaskList {
 
     pub fn remove_task(&mut self, index: usize) -> TaskItem {
         self.tasks.remove(index)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Program {
+    tasks_lists: std::collections::HashMap<String, TaskList>,
+}
+
+impl Program {
+    // pub fn default() -> Self {
+    //     Self {
+    //         tasks_lists: HashMap::default(),
+    //     }
+    // }
+
+    pub fn load_from_json_file(file: &std::fs::File) -> Result<Self, serde_json::Error> {
+        serde_json::from_reader(std::io::BufReader::new(file))
+    }
+
+    pub fn add_list(&mut self, list_name: String, list: TaskList) -> Option<TaskList> {
+        self.tasks_lists.insert(list_name, list)
+    }
+
+    pub fn add_task(&mut self, list_name: &String, task: TaskItem) -> Option<()> {
+        Some(self.tasks_lists.get_mut(list_name)?.add_task(task))
+    }
+
+    pub fn remove_list(&mut self, list_name: &String) -> Option<TaskList> {
+        self.tasks_lists.remove(list_name)
+    }
+
+    pub fn remove_task(&mut self, list_name: &String, task_index: usize) -> Option<TaskItem> {
+        Some(self.tasks_lists.get_mut(list_name)?.remove_task(task_index))
+    }
+
+    pub fn format_task_list(&self, list_name: &String, depth: usize) -> Option<String> {
+        Some(format!(
+            "{}: \n{}\n",
+            list_name,
+            self.tasks_lists.get(list_name)?.format_tasks(depth)
+        ))
+    }
+
+    pub fn format_all_tasks(&self, depth: usize) -> Option<String> {
+        Some(
+            self.tasks_lists
+                .iter()
+                .map(|(list_name, list)| format!("{}: \n{}\n", list_name, list.format_tasks(depth)))
+                .collect(),
+        )
     }
 }
