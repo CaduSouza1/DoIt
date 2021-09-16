85367f38132da3837498e83abe174df653c989e3
Carlos <cadusouza14@gmail.com>
2021-09-16 18:48:53 -0300

    CRemoved the old functions and replaced them with other ones that are more flexible

--- src/cli.rs
+++ src/cli.rs
@@ -1,39 +1,30 @@
 use crate::tasks;
 
-use tasks::{TaskItem, TaskList};
+use tasks::{Program, TaskItem, TaskList};
 
-pub fn parse_add_list_command(
+pub fn get_list_name(matches: &clap::ArgMatches) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(matches
+        .value_of("name")
+        .ok_or("No list name given")?
+        .to_string())
+}
+
+pub fn get_task_title(matches: &clap::ArgMatches) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(matches
+        .value_of("title")
+        .ok_or("No title given")?
+        .to_string())
+}
+
+pub fn get_task_description(
     matches: &clap::ArgMatches,
-) -> Result<(String, TaskList), Box<dyn std::error::Error>> {
-    Ok((
-        matches
-            .value_of("name")
-            .ok_or("No list name given")?
-            .to_string(),
-        TaskList::new(&[]),
-    ))
+) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(matches
+        .value_of("description")
+        .ok_or("No description given")?
+        .to_string())
 }
 
-pub fn parse_add_task_command(
-    matches: &clap::ArgMatches,
-) -> Result<(String, TaskItem), Box<dyn std::error::Error>> {
-    Ok((
-        matches
-            .value_of("list")
-            .ok_or("No list name given")?
-            .to_string(),
-        TaskItem::new(
-            matches.value_of("title").ok_or("No title given")?,
-            matches.value_of("description").ok_or("")?,
-            false,
-        ),
-    ))
-}
-
-pub fn remove_task_command(matches: &clap::ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
-    Ok(())
-}
-
-pub fn remove_list_command(matches: &clap::ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
-    Ok(())
+pub fn get_task_index(matches: &clap::ArgMatches) -> Result<usize, Box<dyn std::error::Error>> {
+    Ok(matches.value_of("index").ok_or("No index given")?.parse()?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,60 @@
+#[macro_use]
+
 mod cli;
 mod tasks;
 
+extern crate clap;
+
 // use std::io::Write;
-use std::{collections::HashMap, fs::File, io::BufReader};
+use std::collections::HashMap;
 use tasks::{Program, TaskItem, TaskList};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = clap::App::new("doit")
-        .subcommand(
-            clap::SubCommand::with_name("add")
-                .subcommand(
-                    clap::SubCommand::with_name("list")
-                        .arg(clap::Arg::with_name("name").short("n").takes_value(true)),
-                )
-                .subcommand(
-                    clap::SubCommand::with_name("task")
-                        .arg(clap::Arg::with_name("title").short("t").takes_value(true))
-                        .arg(
-                            clap::Arg::with_name("description")
-                                .short("d")
-                                .takes_value(true),
-                        )
-                        .arg(clap::Arg::with_name("list").short("l").takes_value(true)),
-                ),
-        )
-        .subcommand(
-            clap::SubCommand::with_name("rm")
-                .subcommand(
-                    clap::SubCommand::with_name("task")
-                        .arg(clap::Arg::with_name("list").short("l").takes_value(true))
-                        .arg(clap::Arg::with_name("index").short("i").takes_value(true)),
-                )
-                .subcommand(
-                    clap::SubCommand::with_name("list")
-                        .arg(clap::Arg::with_name("name").short("n").takes_value(true)),
-                ),
-        )
-        .subcommand(
-            clap::SubCommand::with_name("show")
-                .subcommand(
-                    clap::SubCommand::with_name("list")
-                        .arg(clap::Arg::with_name("name").short("n").takes_value(true)),
-                )
-                .subcommand(clap::SubCommand::with_name("all")),
-        );
+    let yaml = clap::load_yaml!("cli.yml");
+    let app = clap::App::from_yaml(yaml);
 
-    // let filepath = "tasks.json";
-
-    // let mut program = Program::from_json_file(&File::open(&filepath)?)?;
     let mut program = Program::new(&HashMap::default());
     program.add_list(&"b".to_string(), &TaskList::new(&[]));
 
     match app.get_matches().subcommand() {
         ("add", Some(add_matches)) => match add_matches.subcommand() {
             ("task", Some(task_matches)) => {
-                let (list_name, task) = cli::parse_add_task_command(task_matches)?;
-
                 program
-                    .add_task(&list_name, &task)
-                    .expect(&format!("No list with name \"{}\" found", list_name));
+                    .add_task(
+                        &cli::get_list_name(task_matches)?,
+                        &TaskItem::new(
+                            &cli::get_task_title(task_matches)?,
+                            &cli::get_task_description(task_matches)?,
+                            false,
+                        ),
+                    )
+                    .expect("No list with given name found");
             }
             ("list", Some(list_matches)) => {
-                let (list_name, list) = cli::parse_add_list_command(list_matches)?;
-
-                program.add_list(&list_name, &list);
+                program.add_list(&cli::get_list_name(list_matches)?, &TaskList::new(&[]));
             }
             ("", None) => {}
             _ => unreachable!(),
         },
         ("rm", Some(rm_matches)) => match rm_matches.subcommand() {
-            ("task", Some(task_matches)) => {}
-            ("list", Some(list_matches)) => {}
+            ("task", Some(task_matches)) => {
+                program.remove_task(
+                    &cli::get_list_name(task_matches)?,
+                    cli::get_task_index(task_matches)?,
+                );
+            }
+            ("list", Some(list_matches)) => {
+                program.remove_list(&cli::get_list_name(list_matches)?);
+            }
             ("", None) => {}
             _ => unreachable!(),
         },
-       ("show", Some(show_matches)) => match show_matches.subcommand() {
-           ("list", Some(list_matches)) => {},
-           ("all", Some(all_matches)) => {},
+        ("show", Some(show_matches)) => match show_matches.subcommand() {
+            ("list", Some(list_matches)) => {}
+            ("all", Some(all_matches)) => {}
             ("", None) => {}
             _ => unreachable!(),
-        }
+        },
         ("", None) => {}
         _ => unreachable!(),
     }
