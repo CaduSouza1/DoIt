719194ed1ade9fddf69edf2aee3903938fd672b2
CaduSouza1 <cadusouza14@gmail.com>
2022-01-06 22:48:03 -0300

    Changed how the tasks are saved

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ mod cli;
 mod tasks;
 
 use app::create_app;
+use std::path::Path;
 use std::{fs::File, io::Write};
 use tasks::TaskLists;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let filepath = "tasks.json";
-
     let app = create_app();
+    let matches = app.get_matches();
+    let default_file = Path::new("tasks.json"); // default file for read and write operations
+    let mut task_lists = TaskLists::from_json_file(&File::open(&default_file)?)?;
 
-    let mut task_lists = TaskLists::from_json_file(&File::open(&filepath)?)?;
-
-    match app.get_matches().subcommand() {
+    match matches.subcommand() {
         ("add", Some(add_matches)) => cli::parse_add_command(add_matches, &mut task_lists),
         ("remove", Some(rm_matches)) => cli::parse_remove_command(rm_matches, &mut task_lists)?,
         ("show", Some(show_matches)) => cli::parse_show_command(show_matches, &task_lists),
@@ -23,6 +23,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("", None) => (),
         _ => unreachable!(),
     };
-    File::create("tasks.json")?.write_all(serde_json::to_string_pretty(&task_lists)?.as_bytes())?;
+
+    let save_file = matches
+        .value_of("file")
+        .or(Some(default_file.to_str().unwrap()))
+        .unwrap();
+
+    File::create(save_file)?.write_all(serde_json::to_string_pretty(&task_lists)?.as_bytes())?;
     Ok(())
 }
