bf5a8821c0fc870bf3b57498594f0abb1e6bf532
CaduSouza1 <cadusouza14@gmail.com>
2022-04-03 22:49:59 -0300

    Changed the way the app is created

--- src/app.rs
+++ src/app.rs
@@ -1,166 +1,79 @@
-use clap::{App, Arg, SubCommand};
+use std::path::PathBuf;
 
-pub fn create_app<'a, 'b>() -> App<'a, 'b> {
-    App::new("doit")
-        .arg(
-            Arg::with_name("save-file")
-                .short("s")
-                .takes_value(true)
-                .help("Sets the file where the data will be stored"),
-        )
-        .arg(
-            Arg::with_name("read-file")
-                .short("r")
-                .takes_value(true)
-                .help("Sets the file where the data will be read"),
-        )
-        .arg(
-            Arg::with_name("file")
-                .short("f")
-                .takes_value(true)
-                .help("Sets the file where the data will be read and write to. This option does not take precedence over the other io options"),
-        )
-        .subcommands(vec![
-            SubCommand::with_name("add")
-                .about("Adds tasks to existing lists or creates new empty lists")
-                .alias("a")
-                .subcommands(vec![
-                    SubCommand::with_name("task")
-                        .about("Adds a task to an existing list")
-                        .args(&[
-                            Arg::with_name("title")
-                                .short("t")
-                                .takes_value(true)
-                                .help("Sets the name of the task to be created")
-                                .required(true),
-                            Arg::with_name("description")
-                                .short("d")
-                                .takes_value(true)
-                                .help("Sets the description of the task to be created")
-                                .default_value(""),
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list where the task will be stored")
-                                .required(true),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the task will be stored in the list")
-                                .required(false)
-                                .default_value("0"),
-                        ]),
-                    SubCommand::with_name("list")
-                        .about("Creates a new empty task list")
-                        .args(&[
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the name of the list to be created")
-                                .required(true),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the list will be stored")
-                                .default_value("0"),
-                        ]),
-                ]),
-            SubCommand::with_name("remove")
-                .about("Removes tasks from existing lists or removes an entire list")
-                .alias("r")
-                .subcommands(vec![
-                    SubCommand::with_name("task")
-                        .about("Removes a task from an existing list")
-                        .alias("t")
-                        .args(&[
-                            Arg::with_name("title")
-                                .short("t")
-                                .takes_value(true)
-                                .help("Sets the name of the task to be deleted"),
-                            Arg::with_name("description")
-                                .short("d")
-                                .takes_value(true)
-                                .help("Sets the description of the task to be deleted"),
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list where the task will be deleted"),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the task is stored for deletion"),
-                        ]),
-                    SubCommand::with_name("list")
-                        .about("Removes an entire list")
-                        .alias("l")
-                        .args(&[
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list name to be deleted"),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the list is stored for deletion"),
-                        ]),
-                ]),
-            SubCommand::with_name("show")
-                .about("Shows task information of a list")
-                .alias("s")
-                .subcommands(vec![
-                    SubCommand::with_name("list")
-                        .about("Shows the information of all tasks of a specific list")
-                        .args(&[
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list name to be shown"),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the list is stored to be shown"),
-                        ]),
-                    SubCommand::with_name("all")
-                        .about("Shows the information of all tasks of all lists"),
-                ]),
-            SubCommand::with_name("complete")
-                .alias("c")
-                .about("Marks a task or an entire list as completed")
-                .subcommands(vec![
-                    SubCommand::with_name("list")
-                        .about("Marks all tasks in a list as completed")
-                        .args(&[
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list name to mark all tasks as completed"),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the list is stored"),
-                        ]),
-                    SubCommand::with_name("task")
-                        .about("Marks a task as completed")
-                        .alias("t")
-                        .args(&[
-                            Arg::with_name("title")
-                                .short("t")
-                                .takes_value(true)
-                                .help("Sets the name of the task to be marked as completed"),
-                            Arg::with_name("description")
-                                .short("d")
-                                .takes_value(true)
-                                .help("Sets the description of the task to be marked as completed"),
-                            Arg::with_name("list")
-                                .short("l")
-                                .takes_value(true)
-                                .help("Sets the list where the task will be marked as completed")
-                                .required(true),
-                            Arg::with_name("index")
-                                .short("i")
-                                .takes_value(true)
-                                .help("Sets the index where the task is stored"),
-                        ]),
-                ]),
-        ])
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+pub struct Cli {
+    #[clap(short, long, value_name = "CONFIG-FILE")]
+    pub config: Option<PathBuf>,
+
+    #[clap(short, long, value_name = "TASKS-FILE")]
+    pub tasks_filepath: Option<PathBuf>,
+
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Creates a new list
+    NewList {
+        #[clap(short, long)]
+        name: String,
+    },
+
+    /// Adds a task to a list
+    AddTask {
+        #[clap(short, long)]
+        list: String,
+
+        #[clap(short, long)]
+        title: String,
+
+        #[clap(short, long)]
+        description: String,
+
+        #[clap(short, long)]
+        completed: Option<bool>,
+    },
+
+    /// Removes a list and all of its items
+    RemoveList {
+        #[clap(short, long)]
+        name: String,
+    },
+
+    /// Removes a task from a list
+    RemoveTask {
+        #[clap(short, long)]
+        list_name: String,
+
+        #[clap(short, long)]
+        index: usize,
+    },
+
+    /// Marks a task as completed
+    CompleteTask {
+        #[clap(short, long)]
+        list_name: String,
+
+        #[clap(short, long)]
+        index: usize,
+    },
+
+    /// Marks all tasks of a list as completed
+    CompleteList {
+        #[clap(short, long)]
+        name: String,
+    },
+
+    /// Prints the tasks of a list to stdout
+    ShowList {
+        #[clap(short, long)]
+        name: String,
+    },
+
+    /// Prints all lists to stdout
+    ShowAll {},
 }
